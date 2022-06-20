3759334b504b144eff539a29db9488fbf8c40099
ZoeMRuf <92019231+ZoeMRuf@users.noreply.github.com>
2022-06-20 17:56:45 +0200

    ..

---
+++ Frontend/firebase_auth/module_auth_stateChanged.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+import { getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDtDXXiHIAyEcraPlIcJhxCsWofuetO-pU",
+    authDomain: "helloworld-ea756.firebaseapp.com",
+    projectId: "helloworld-ea756",
+    storageBucket: "helloworld-ea756.appspot.com",
+    messagingSenderId: "421368849663",
+    appId: "1:421368849663:web:7f6cb76ffde540444258ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+
+//Listern for changes
